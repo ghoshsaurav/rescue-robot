c9c2e09f8add53c2af77847901f14b7db9b41ec5
Saurav Ghosh <71978046+ghoshsaurav@users.noreply.github.com>
2026-09-09 17:31:54 -0500

    Implement addition warmup

---
+++ warmup.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+def add(a, b):
+    """Return the result of adding a and b.
+
+    The autograder checks that you return the value, not print it.
+    """
+    return a + b
